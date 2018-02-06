aa92b0a788c130457eec60a8e41567f81caa69ae
iBug <7273074+iBug@users.noreply.github.com>
2018-02-07 00:01:08 +0800

    Add more supported rooms

--- targetBlame.meta.js
+++ targetBlame.meta.js
@@ -1,11 +1,14 @@
 // ==UserScript==
 // @name         !!/blame your target
-// @version      1.0.1
+// @version      1.0.2
 // @description  Generate targeted "!!/blame" command
 // @author       iBug
 // @updateURL    https://raw.githubusercontent.com/iBug/userscript/master/targetBlame.meta.js
 // @downloadURL  https://raw.githubusercontent.com/iBug/userscript/master/targetBlame.user.js
-// @match        https://chat.stackexchange.com/rooms/11540/charcoal-hq
+// @match        *://chat.stackexchange.com/rooms/11540/*
+// @match        *://chat.stackoverflow.com/rooms/41570/*
+// @match        *://chat.stackoverflow.com/rooms/111347/*
+// @match        *://chat.meta.stackexchange.com/rooms/89/*
 // @run-at       document-end
 // @grant        none
 // ==/UserScript==
