0333942f208a0c648039f34683e1d1d839583587
iBug <7273074+iBug@users.noreply.github.com>
2018-02-06 23:04:35 +0800

    Add update info

---
+++ targetBlame.meta.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         !!/blame your target
+// @version      1.0
+// @description  Generate targeted "!!/blame" command
+// @author       iBug
+// @updateURL    https://raw.githubusercontent.com/iBug/userscript/master/targetBlame.meta.js
+// @downloadURL  https://raw.githubusercontent.com/iBug/userscript/master/targetBlame.user.js
+// @match        https://chat.stackexchange.com/rooms/11540/charcoal-hq
+// @run-at       document-end
+// @grant        none
+// ==/UserScript==
--- targetBlame.user.js
+++ targetBlame.user.js
@@ -3,6 +3,8 @@
 // @version      1.0
 // @description  Generate targeted "!!/blame" command
 // @author       iBug
+// @updateURL    https://raw.githubusercontent.com/iBug/userscript/master/targetBlame.meta.js
+// @downloadURL  https://raw.githubusercontent.com/iBug/userscript/master/targetBlame.user.js
 // @match        https://chat.stackexchange.com/rooms/11540/charcoal-hq
 // @run-at       document-end
 // @grant        none
