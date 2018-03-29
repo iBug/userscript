386216063d380844a963656ee2f4081bb441b96d
iBug <7273074+iBug@users.noreply.github.com>
2018-03-29 13:12:27 +0800

    .

--- bailian-all-ac.user.js
+++ bailian-all-ac.user.js
@@ -9,7 +9,7 @@
 // @grant        none
 // ==/UserScript==
 
-((frame) => {
+((document, window, console) => {
     'use strict';
 
     const clog = (s) => console.log("[iBug.All-AC] " + s);
@@ -19,8 +19,7 @@
 
     // Utilities
     var url = window.location.href;
-    var randSeed = Number(/solution\/(\d*)/g.exec(url)[1]);
-    clog("Extracted submission ID: " + String(randSeed));
+    var randSeed = 0;
 
     const randInt = function(a, b) {
         randSeed = ((randSeed * 214013) + 2531011) & 0xFFFFFFFF;
@@ -28,15 +27,8 @@
         return Math.floor(rnd * (b-a)) + a;
     };
 
-    // Initialize stuff
-    var mainSection = frame.getElementById("main");
-    if (mainSection == undefined) {
-        // It's a typo, who knows...
-        mainSection = frame.getElementById("main\"");
-    }
-
     // Try to identify current user from toolbar
-    var userToolbar = frame.getElementById("userToolbar");
+    var userToolbar = document.getElementById("userToolbar");
     {
         let items = userToolbar.getElementsByTagName("a");
         let userLink;
@@ -56,96 +48,138 @@
         }
     }
 
-    // Change result to AC
-    var res = frame.querySelectorAll('p[class="compile-status"]')[0].querySelectorAll('a')[0];
-    if (res != undefined) {
-        clog("Detected original result: " + res.text);
-        res.className = "result-right";
-        res.href = res.href.replace(/\/*#.*$/, "");
-        res.text = "Accepted";
-    }
+    const submissionPage = function(document) {
+        clog("Running on submission page");
 
-    // Remove Compiler error information if exist
-    res = frame.querySelectorAll('h3[class="h3-compile-status"]')[0];
-    if (res != undefined) {
-        clog("Detected compiler message, removing...");
-        res.nextElementSibling.remove();  // The <pre> block
-        res.remove();
-        res = frame.querySelectorAll('a[name="compile-error"]')[0];
+        // Initialize stuff
+        var mainSection = document.getElementById("main");
+        if (mainSection == undefined) {
+            // It's a typo, who knows...
+            mainSection = document.getElementById("main\"");
+        }
+        randSeed = Number(/solution\/(\d*)/g.exec(url)[1]);
+        clog("Extracted submission ID: " + String(randSeed));
+
+        // Change result to AC
+        var res = document.querySelectorAll('p[class="compile-status"]')[0].querySelectorAll('a')[0];
         if (res != undefined) {
-            res.remove();
-        }
-    }
-
-    // Insert running information if they're missing
-    res = frame.querySelectorAll('div[class="compile-info"]')[0];
-    var fakeTime = randInt(100,2000), fakeMem = randInt(1000,10000), submissionTime,
-        memText = String(fakeMem) + "kB", timeText = String(fakeTime) + "ms";
-    {
-        let entries = res.querySelectorAll('dt');
-        let found;
-        for (let i = 0; i < entries.length; i++){
-            if (entries[i].innerText == "提交人:") {
-                found = entries[i];
-                break;
-            }
-        }
-        if (found != undefined) {
-            let target = found.nextElementSibling.nextElementSibling, elm;
-            if (target != undefined && target.innerText == "语言:") {
-                clog("Detected missing mem info and time info.");
-                clog("Filling with mem=" + memText + " and time=" + timeText);
-                elm = frame.createElement("dt");
-                elm.innerText = "内存:";
-                target.parentElement.insertBefore(elm, target);
-                elm = frame.createElement("dd");
-                elm.innerText = memText;
-                target.parentElement.insertBefore(elm, target);
-
-                elm = frame.createElement("dt");
-                elm.innerText = "时间:";
-                target.parentElement.insertBefore(elm, target);
-                elm = frame.createElement("dd");
-                elm.innerText = timeText;
-                target.parentElement.insertBefore(elm, target);
-            }
-        }
-
-        // Obtain submission time
-        found = undefined;
-        for (let i = 0; i < entries.length; i++){
-            if (entries[i].innerText == "提交时间:") {
-                found = entries[i];
-                break;
-            }
-        }
-        if (found != undefined) {
-            submissionTime = found.nextElementSibling.innerText;
-            clog("Detected submission time: " + submissionTime);
-        }
-    }
-
-    // Process "recent submits" section
-    var submits = mainSection.getElementsByTagName("table")[0].getElementsByTagName("tbody")[0].getElementsByTagName("tr");
-    for (let i = 0; i < submits.length; i++) {
-        let submit = submits[i];
-        let items = submit.getElementsByTagName("td");
-        if (submit.querySelectorAll('td[class="submit-user"]')[0].getElementsByTagName("a")[0].innerText == thisUser &&
-            submit.querySelectorAll('td[class="date"]')[0].getElementsByTagName("abbr")[0].title == submissionTime) {
-            let res;
-            // Change result
-            res = submit.querySelectorAll('td[class="result"]')[0].getElementsByTagName("a")[0];
-            res.className  = "result-right";
+            clog("Detected original result: " + res.text);
+            res.className = "result-right";
             res.href = res.href.replace(/\/*#.*$/, "");
-            res.innerText = "Accepted";
-            // Change Mem
-            res = submit.querySelectorAll('td[class="memory"]')[0];
-            if (res.innerText === "")
-                res.innerText = String(fakeMem) + "kB";
-            // Change Time
-            res = submit.querySelectorAll('td[class="spending-time"]')[0];
-            if (res.innerText === "")
-                res.innerText = String(fakeTime) + "ms";
+            res.text = "Accepted";
         }
+
+        // Remove Compiler error information if exist
+        res = document.querySelectorAll('h3[class="h3-compile-status"]')[0];
+        if (res != undefined) {
+            clog("Detected compiler message, removing...");
+            res.nextElementSibling.remove();  // The <pre> block
+            res.remove();
+            res = document.querySelectorAll('a[name="compile-error"]')[0];
+            if (res != undefined) {
+                res.remove();
+            }
+        }
+
+        // Insert running information if they're missing
+        res = document.querySelectorAll('div[class="compile-info"]')[0];
+        var fakeTime = randInt(100,2000), fakeMem = randInt(1000,10000), submissionTime,
+            memText = String(fakeMem) + "kB", timeText = String(fakeTime) + "ms",
+            sidebarTitle = res.firstElementChild, sidebarMem, sidebarTime, recentsMem, recentsTime;
+        {
+            let entries = res.querySelectorAll('dt'),
+                found;
+            for (let i = 0; i < entries.length; i++){
+                if (entries[i].innerText == "提交人:") {
+                    found = entries[i];
+                    break;
+                }
+            }
+            if (found != undefined) {
+                let target = found.nextElementSibling.nextElementSibling, elm;
+                if (target != undefined && target.innerText == "语言:") {
+                    clog("Detected missing mem info and time info.");
+                    clog("Filling with mem=" + memText + " and time=" + timeText);
+                    elm = document.createElement("dt");
+                    elm.innerText = "内存:";
+                    target.parentElement.insertBefore(elm, target);
+                    elm = document.createElement("dd");
+                    elm.innerText = memText;
+                    target.parentElement.insertBefore(elm, target);
+                    sidebarMem = elm;
+
+                    elm = document.createElement("dt");
+                    elm.innerText = "时间:";
+                    target.parentElement.insertBefore(elm, target);
+                    elm = document.createElement("dd");
+                    elm.innerText = timeText;
+                    target.parentElement.insertBefore(elm, target);
+                    sidebarTime = elm;
+                } else if (target != undefined && target.innerText == "内存:") {
+                    let memDT = target,
+                        memDD = memDT.nextElementSibling;
+                    let timeDT = memDT.nextElementSibling.nextElementSibling,
+                        timeDD = timeDT.nextElementSibling;
+                    sidebarMem = memDD;
+                    sidebarTime = timeDD;
+                }
+            }
+
+            // Obtain submission time
+            found = undefined;
+            for (let i = 0; i < entries.length; i++){
+                if (entries[i].innerText == "提交时间:") {
+                    found = entries[i];
+                    break;
+                }
+            }
+            if (found != undefined) {
+                submissionTime = found.nextElementSibling.innerText;
+                clog("Detected submission time: " + submissionTime);
+            }
+        }
+
+        // Process "recent submits" section
+        var submits = mainSection.getElementsByTagName("table")[0].getElementsByTagName("tbody")[0].getElementsByTagName("tr");
+        for (let i = 0; i < submits.length; i++) {
+            let submit = submits[i];
+            let items = submit.getElementsByTagName("td");
+            if (submit.querySelectorAll('td[class="submit-user"]')[0].getElementsByTagName("a")[0].innerText == thisUser &&
+                submit.querySelectorAll('td[class="date"]')[0].getElementsByTagName("abbr")[0].title == submissionTime) {
+                let res;
+                // Change result
+                res = submit.querySelectorAll('td[class="result"]')[0].getElementsByTagName("a")[0];
+                res.className  = "result-right";
+                res.href = res.href.replace(/\/*#.*$/, "");
+                res.innerText = "Accepted";
+                // Change Mem
+                recentsMem = submit.querySelectorAll('td[class="memory"]')[0];
+                if (recentsMem.innerText === "")
+                    recentsMem.innerText = String(fakeMem) + "kB";
+                // Change Time
+                recentsTime = submit.querySelectorAll('td[class="spending-time"]')[0];
+                if (recentsTime.innerText === "")
+                    recentsTime.innerText = String(fakeTime) + "ms";
+            }
+        }
+
+        sidebarTitle.onclick = () => {
+            if (sidebarMem != undefined) {
+                sidebarMem.innerText = memText;
+            }
+            if (sidebarTime != undefined) {
+                sidebarTime.innerText = timeText;
+            }
+            if (recentsMem != undefined) {
+                recentsMem.innerText = memText;
+            }
+            if (recentsTime != undefined) {
+                recentsTime.innerText = timeText;
+            }
+        };
+    };
+
+    if (url.startsWith("http://bailian.openjudge.cn/practice/solution/") || url.startsWith("https://bailian.openjudge.cn/practice/solution/")) {
+        submissionPage(document);
     }
-})(document);
+})(document, window, console);
