3b94dfc04a05acac9880ce461927de0887742050
iBug <git@ibugone.com>
2023-09-21 15:32:07 +0800

    github-custom-style: Use GM_addElement

--- github-custom-style.user.js
+++ github-custom-style.user.js
@@ -3,13 +3,13 @@
 // @description      My Custom Styles for GitHub
 // @author           iBug
 // @namespace        https://github.com/iBug/userscript
-// @version          1.0.1
+// @version          1.0.4
 // @updateURL        https://raw.githubusercontent.com/iBug/userscript/master/github-custom-style.user.js
 // @downloadURL      https://raw.githubusercontent.com/iBug/userscript/master/github-custom-style.user.js
 // @match            *://github.com/*
 // @match            *://*.github.com/*
 // @run-at           document-start
-// @grant            none
+// @grant            GM_addElement
 // ==/UserScript==
 
 // SEMI-IMPORTANT: If you wish to avoid flickering on pageload, this script must run before the page content loads
@@ -18,15 +18,15 @@
 
 'use strict';
 
-let e = document.createElement("link");
-e.rel = "stylesheet";
-e.href = "https://cdn.jsdelivr.net/npm/amazon-fonts@1.0.1/fonts/stylesheet.min.css";
-(document.head || document.documentElement).appendChild(e);
+GM_addElement(document.head || document.documentElement, "link", {
+  rel: "stylesheet",
+  href: "https://cdn.jsdelivr.net/npm/amazon-fonts@1.0.1/fonts/stylesheet.min.css",
+});
 
 (document.head || document.documentElement).appendChild(document.createElement('style')).textContent = `
 body {
   --ff-sans: Amazon Ember,Microsoft YaHei UI,Segoe UI,Helvetica,Arial,sans-serif,Apple Color Emoji,Segoe UI Emoji !important;
-  --ff-mono: Amazon Ember Mono,Consolas,Roboto Mono,monospace !important;
+  --ff-mono: Fira Code iBug,Fira Code,Amazon Ember Mono,Consolas,Roboto Mono,monospace !important;
 }
 
 body, .markdown-body {
@@ -34,7 +34,8 @@ body, .markdown-body {
 }
 
 code, pre, tt, .text-mono,
-.blob-num, .blob-code-inner {
+.blob-num, .blob-code-inner,
+.react-code-text {
   font-family: var(--ff-mono) !important;
 }
 `;
